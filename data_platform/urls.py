b5dde57e83b950796f2d4339bfbfc12a4a564598
Kianoush  Arshi <kianoosharshi@gmail.com>
2021-08-11 12:07:35 +0430

    Homepage

--- data_platform/urls.py
+++ data_platform/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('workflows/', include('workflows.urls')),
     path('admin/', admin.site.urls),
 
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
 
     path('datasets/', include('datasets.urls')),
