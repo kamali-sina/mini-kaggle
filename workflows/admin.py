214aae5b6b503e36f0ec33aeac055f8958b446ac
Kianoush  Arshi <kianoosharshi@gmail.com>
2021-08-15 18:15:57 +0430

    Add workflow model

--- workflows/admin.py
+++ workflows/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from workflows.models import PythonTask, Task
+from workflows.models import PythonTask, Task, Workflow
 
 # Register your models here.
 
 admin.site.register(Task)
 admin.site.register(PythonTask)
+admin.site.register(Workflow)
