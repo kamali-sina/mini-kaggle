e96273a09d7d5ce71e35add744bb48511be855f1
Mahta Fetrat <77fetrat@gmail.com>
2021-08-15 13:43:44 +0430

    fix bug of logout page improper link to login

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-
 from . import views
+from django.contrib.auth import views as auth_views
+
+from django.urls import path, include
 
 app_name = 'users'
 urlpatterns = [
-    path('users/signup', views.Signup.as_view(), name='signup'),
-    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('signup/', views.Signup.as_view(), name='signup'),
 ]
