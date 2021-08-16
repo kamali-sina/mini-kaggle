9378c97e8798aaea171cee0c4e121a57d52ffa8d
Mahta Fetrat <77fetrat@gmail.com>
2021-08-16 21:35:04 +0430

    add search and filter using query params

---
+++ datasets/templatetags/filter_query_params.py
@@ -0,0 +1,32 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def add_tag_param(request, tag):
+    query_dict = request.GET.copy()
+    query_dict.appendlist('tag', tag)
+    return "?" + query_dict.urlencode()
+
+
+@register.simple_tag
+def remove_tag_param(request, tag):
+    query_dict = request.GET.copy()
+    tags = query_dict.getlist('tag')
+    tags.remove(tag)
+    query_dict.setlist('tag', tags)
+    if 'page' in query_dict:
+        query_dict['page'] = 1
+    return "?" + query_dict.urlencode()
+
+
+@register.simple_tag
+def get_tag_params(request):
+    return request.GET.getlist('tag')
+
+
+@register.simple_tag
+def append_params(request):
+    query_dict = request.GET.copy()
+    return "&" + query_dict.urlencode() if query_dict else ''
