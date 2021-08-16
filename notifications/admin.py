473b83996174b3782203375f6e5d4b8d713d0eed
Mahta Fetrat <77fetrat@gmail.com>
2021-08-16 18:36:51 +0430

    Change notification source models inheritance from abstract to multi-table

--- notifications/admin.py
+++ notifications/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from notifications.models import EmailNotificationSource
+from notifications.models import NotificationSource, EmailNotificationSource
 
+admin.site.register(NotificationSource)
 admin.site.register(EmailNotificationSource)
--- notifications/models.py
+++ notifications/models.py
@@ -1,17 +1,32 @@
+from enum import Enum
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
 
 class NotificationSource(models.Model):
+    class NotificationSourceType(models.TextChoices):
+        EMAIL = 'EM', _("Email")
+
     title = models.CharField(max_length=200, default='Untitled')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.CharField(max_length=5, choices=NotificationSourceType.choices, default=NotificationSourceType.EMAIL)
 
-    class Meta:
-        abstract = True
+    def cast(self):
+        return getattr(self, NOTIFICATION_CHILD_REGISTRY[self.type])
+
+    def __str__(self):
+        return self.title
 
 
 class EmailNotificationSource(NotificationSource):
     recipients = models.JSONField()
+
+
+NOTIFICATION_CHILD_REGISTRY = {
+    NotificationSource.NotificationSourceType.EMAIL: 'emailnotificationsource'
+}
