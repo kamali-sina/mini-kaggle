fa789a0648086127bf67e017bc184b9adbd6657f
Mahta Fetrat <77fetrat@gmail.com>
2021-08-17 09:55:21 +0430

    fix logout page link to login

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 app_name = 'users'
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('', include('django.contrib.auth.urls')),
     path('signup/', views.Signup.as_view(), name='signup'),
 ]
