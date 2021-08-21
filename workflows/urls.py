900d55077583bef5ee120192e3dd79d9054d6cd3
Kianoush  Arshi <kianoosharshi@gmail.com>
2021-08-21 23:00:12 +0430

    Workflow crud bugfix

--- workflows/urls.py
+++ workflows/urls.py
@@ -4,20 +4,20 @@ from . import views
 app_name = 'workflows'
 
 workflows_urls = [
-    path('workflows/<int:pk>/', views.WorkflowDetailView.as_view(), name='detail_workflow'),
-    path('workflows/<int:pk>/delete/', views.WorkflowDeleteView.as_view(), name='delete_workflow'),
+    path('', views.WorkflowListView.as_view(), name='list_workflow'),
+    path('create/', views.WorkflowCreateView.as_view(), name='create_workflow'),
+    path('<int:pk>/', views.WorkflowDetailView.as_view(), name='detail_workflow'),
+    path('<int:pk>/delete/', views.WorkflowDeleteView.as_view(), name='delete_workflow'),
 ]
 
 tasks_urls = [
+    path('tasks/', views.TaskListView.as_view(), name='list_task'),
+    path('tasks/create/', views.TaskCreateView.as_view(), name='create_task'),
     path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='detail_task'),
     path('tasks/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='delete_task'),
 ]
 
 urlpatterns = [
-    path('tasks/', views.TaskListView.as_view(), name='list_task'),
-    path('tasks/create/', views.TaskCreateView.as_view(), name='create_task'),
-    path('workflows/', views.WorkflowListView.as_view(), name='list_workflow'),
-    path('workflows/create/', views.WorkflowCreateView.as_view(), name='create_workflow'),
     *tasks_urls,
     *workflows_urls,
 ]
