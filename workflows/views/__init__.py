bde88f0d27332cf284f91455a3853982c4e4f0d3
Mahta Fetrat <77fetrat@gmail.com>
2021-08-25 11:21:22 +0430

    add workflow schedule forms

--- workflows/views/__init__.py
+++ workflows/views/__init__.py
@@ -1,3 +1,3 @@
 from .task import TaskListView, TaskCreateView, TaskDeleteView, TaskDetailView
-from .workflow import WorkflowListView, WorkflowCreateView, WorkflowDeleteView, WorkflowDetailView
+from .workflow import WorkflowListView, WorkflowCreateView, WorkflowDeleteView, WorkflowDetailView, WorkflowScheduleRedirectView
 from .secret import SecretListView, SecretCreateView, SecretDeleteView, SecretDetailView, SecretCreatorOnlyMixin
