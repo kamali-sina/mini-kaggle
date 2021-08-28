74716d28309ec71ad83dc8b11b782822bdcaea21
Mahta Fetrat <77fetrat@gmail.com>
2021-08-28 17:34:34 +0430

    add notebooks CRUD

---
+++ notebooks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = "notebooks"
+urlpatterns = [
+    path("", views.NotebooksListView.as_view(), name="index"),
+    path("<int:pk>/", views.NotebookDetailView.as_view(), name="detail"),
+    path("<int:pk>/delete", views.NotebookDeleteView.as_view(), name="delete"),
+]
