a29273fc0d6aaaa93c275d911b07d173f8e696ce
Kianoush Arshi <kianoosharshi@gmail.com>
2021-08-30 16:50:27 +0430

    Add time stamps for task executions

--- workflows/models/task.py
+++ workflows/models/task.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import User
@@ -45,3 +46,13 @@ class TaskExecution(models.Model):
     status = models.CharField(max_length=1, choices=StatusChoices.choices, default=StatusChoices.PENDING, db_index=True)
     celery_task_id = models.CharField(max_length=50, null=True, blank=True)
     log = models.FileField(upload_to=task_execution_log_file_directory_path, null=True, blank=True)
+    timestamps = models.JSONField(blank=True, null=True)
+
+    def add_timestamp(self, status_label):
+        timestamp = '$' + status_label + ':' + (datetime.now()).strftime("%m/%d/%Y, %H:%M:%S")
+        self.timestamps
+        self.save()
+
+    def decode_timestamps(self):
+        timestamps = {}
+        self.timestamps.split
--- workflows/services/run_task.py
+++ workflows/services/run_task.py
@@ -1,10 +1,8 @@
 import datetime
 from celery import shared_task
-
 from notifications.services.send_notifications import send_notification
-from workflows.models import TaskExecution
+from workflows.models import TaskExecution, Task
 from workflows.services.runner import get_service_runner
-from workflows.models import Task
 
 
 # pylint: disable=unused-argument
@@ -27,18 +25,26 @@ def run_task_in_celery(task_execution_id):
 
     task_execution.status = TaskExecution.StatusChoices.RUNNING
     task_execution.save()
-
+    task_execution.timestamps[str(TaskExecution.StatusChoices.RUNNING.label)] = (datetime.datetime.now()).strftime(
+        "%m/%d/%Y, %H:%M:%S")
     runner = get_service_runner(task_execution.task)
     try:
         task_execution_status = runner.run_task(task_execution)
     except:  # pylint: disable=bare-except
         task_execution_status = TaskExecution.StatusChoices.FAILED
+    task_execution.timestamps[str(TaskExecution.StatusChoices.FAILED.label)] = (datetime.datetime.now()).strftime(
+        "%m/%d/%Y, %H:%M:%S")
     task_execution.status = task_execution_status
+    task_execution.timestamps[str(task_execution_status.label)] = (datetime.datetime.now()).strftime(
+        "%m/%d/%Y, %H:%M:%S")
     task_execution.save()
 
 
 def run_task(task):
-    task_execution = TaskExecution.objects.create(task=task, status=TaskExecution.StatusChoices.PENDING)
+    task_execution = TaskExecution.objects.create(task=task, status=TaskExecution.StatusChoices.PENDING,
+                                                  timestamps={
+                                                      str(TaskExecution.StatusChoices.PENDING.label): (
+                                                          datetime.datetime.now()).strftime("%m/%d/%Y, %H:%M:%S")})
     if not task.timeout or task.timeout.total_seconds() == 0:
         celery_task = run_task_in_celery.delay(task_execution.pk)
     else:
