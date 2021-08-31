33d33d5c6e651224e3ad46b3a44342ea19c664af
Mahta Fetrat <77fetrat@gmail.com>
2021-08-31 16:26:59 +0430

    add code snippets api

--- notebooks/urls.py
+++ notebooks/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.NotebooksListView.as_view(), name="index"),
     path("<int:pk>/", views.NotebookDetailView.as_view(), name="detail"),
     path("<int:pk>/delete", views.NotebookDeleteView.as_view(), name="delete"),
+    path("snippets", views.snippets_list_view, name="snippets"),
+    path("snippets/<str:name>", views.snippet_detail_view, name="snippet_detail"),
 ]
--- notebooks/views.py
+++ notebooks/views.py
@@ -1,7 +1,10 @@
+import os
+
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 from django.urls import reverse_lazy
-
+from django.http import JsonResponse
+from django.conf import settings
 from notebooks.models import Notebook
 
 
@@ -29,3 +32,15 @@ class NotebookDetailView(LoginRequiredMixin, generic.DetailView):
 class NotebookDeleteView(LoginRequiredMixin, NotebookCreatorOnlyMixin, generic.DeleteView):
     model = Notebook
     success_url = reverse_lazy('notebooks:index')
+
+
+def snippets_list_view(request):
+    filenames = next(os.walk('./notebooks/static/notebooks/snippets'), (None, None, []))[2]
+    values = [{'name': name, 'value': os.path.splitext(name)[0].replace('_', ' ').capitalize()} for name in filenames]
+
+    return JsonResponse({'success': True, 'results': values})
+
+
+def snippet_detail_view(request, name):
+    with open(f'./notebooks/static/notebooks/snippets/{name}', 'r') as file:
+        return JsonResponse({'snippet': file.read()})
