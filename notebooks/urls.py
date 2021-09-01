eb0a7d1211c5f94ba7ff00846d22b57239c9ba86
ali <alinajafi321@gmail.com>
2021-09-01 23:35:14 +0430

    RestartNotebookKernel

--- notebooks/urls.py
+++ notebooks/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "notebooks"
 urlpatterns = [
     path("", views.NotebooksListView.as_view(), name="index"),
+    path("<int:pk>/", views.RestartNotebookKernelView, name="restart_notebook_kernel"),
     path("<int:pk>/", views.NotebookDetailView.as_view(), name="detail"),
     path("<int:pk>/delete", views.NotebookDeleteView.as_view(), name="delete"),
     path("<int:pk>/export", views.ExportNotebook.as_view(), name="export"),
--- notebooks/views.py
+++ notebooks/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 from django.contrib import messages
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 
 from workflows.models import PythonTask
 
+from notebooks.services.session import make_new_session, SessionService
 from notebooks.models import Notebook
 from notebooks.forms import ExportNotebookForm
 
@@ -52,3 +53,16 @@ class ExportNotebook(LoginRequiredMixin, NotebookCreatorOnlyMixin, generic.Creat
         task = form.save()
         messages.success(self.request, 'Your task has been created :)')
         return HttpResponseRedirect(reverse('workflows:detail_task', args=(task.id,)))
+
+
+def RestartNotebookKernelView(request, pk):
+    notebook = Notebook.objects.get(pk=pk)
+
+    if notebook.session_id:
+        session_id = notebook.session_id
+    else:
+        session_id = make_new_session()
+
+    session_service = SessionService(session_id)
+    session_service.restart()
+    return JsonResponse({})
