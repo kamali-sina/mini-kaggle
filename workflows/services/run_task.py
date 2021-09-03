3e4d6462b98bc664298611459d63af1ee3177c9b
Kianoush Arshi <kianoosharshi@gmail.com>
2021-09-03 17:21:54 +0430

    Import datetime

--- workflows/services/run_task.py
+++ workflows/services/run_task.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from celery import shared_task
 from notifications.services.send_notifications import send_notification
 from workflows.models import TaskExecution, Task
@@ -10,7 +10,7 @@ def run_task_on_failure(self, exc, celery_task_id, args, kwargs, einfo):
     task = Task.objects.get(pk=args[0])
     context = {'task': task,
                'error': einfo.traceback,
-               'datetime': datetime.datetime.now()}
+               'datetime': datetime.now()}
 
     if task.alert_on_failure and task.notification_source:
         send_notification(task.notification_source,
@@ -25,14 +25,14 @@ def run_task_in_celery(task_execution_id):
 
     task_execution.status = TaskExecution.StatusChoices.RUNNING
     task_execution.save()
-    start_task_time = datetime.datetime.now()
+    start_task_time = datetime.now()
     runner = get_service_runner(task_execution.task)
     try:
         task_execution_status = runner.run_task(task_execution)
     except:  # pylint: disable=bare-except
         task_execution_status = TaskExecution.StatusChoices.FAILED
     task_execution.status = task_execution_status
-    run_time = (datetime.datetime.now() - start_task_time).seconds
+    run_time = (datetime.now() - start_task_time).seconds
     task_execution.run_time = run_time
     task_execution.save()
 
