d36ec69ae7ace032c78c91989281d06ba38374c5
Mahta Fetrat <77fetrat@gmail.com>
2021-09-04 17:23:42 +0430

    link workflow run to the logic

--- workflows/views/__init__.py
+++ workflows/views/__init__.py
@@ -1,3 +1,4 @@
 from .task import TaskListView, create_task_view, TaskDeleteView, TaskDetailView, get_typed_task_form
-from .workflow import WorkflowListView, WorkflowCreateView, WorkflowDeleteView, WorkflowDetailView, WorkflowScheduleRedirectView
+from .workflow import WorkflowListView, WorkflowCreateView, WorkflowDeleteView, WorkflowDetailView, \
+    WorkflowScheduleRedirectView, WorkflowRunView
 from .secret import SecretListView, SecretCreateView, SecretDeleteView, SecretDetailView, SecretCreatorOnlyMixin
