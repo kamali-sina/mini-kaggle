3cd57d0d14f79db778ae6ccabf92b111dae2ae8d
sinku <sininoir@gmail.com>
2022-09-08 20:39:59 +0430

    Remove data_platform

--- data_platform/static/data_platform/scripts/toast.js
+++
@@ -1,6 +0,0 @@
-function showToast(message) {
-    const toastElement = document.getElementById("toast");
-    toastElement.className = "show";
-    toastElement.innerHTML = message
-    setTimeout(function(){ toastElement.className = toastElement.className.replace("show", ""); }, 3000);
-}
